6379eceaffb13c7b7e4caceae3b361a28054056d
RupaSiddana <roopa.siddana@gmail.com>
2025-06-24 21:41:54 +0530

    special variable

--- special-variable.sh
+++ special-variable.sh
@@ -7,4 +7,4 @@ echo "user running this script:$USER"
 echo "Home directory of users: $HOME"
 echo "PID of the script:$$"
 sleep 10 &
-echo ""PID of last command in background:$!"
+echo "PID of last command in background:$!"
