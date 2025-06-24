f5aeb82288bc27236d96403d31e01dfb3b1a681f
RupaSiddana <roopa.siddana@gmail.com>
2025-06-24 23:00:43 +0530

    install-module

--- install-modules.sh
+++ install-modules.sh
@@ -3,6 +3,7 @@ userid=(id -u)
 if [ $userid -ne 0 ]
 then
    echo "Error:: Please run with root access"
+   exit 1
 else
    echo "You are running with root access"
 fi
@@ -16,9 +17,9 @@ then
       echo "Installing mysql is... sucsess"
    else
       echo "Installing my is ... fail"
+      exit 1
    fi
 else
    echo "Mysql is already isnatlled no action is pending"
-   exit 1
 fi
    
