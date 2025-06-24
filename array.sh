ed85a082dab40a9ccc7032042d6c379163afc99b
RupaSiddana <roopa.siddana@gmail.com>
2025-06-24 21:29:43 +0530

    array2

--- array.sh
+++ array.sh
@@ -4,4 +4,4 @@ movies=("court" "hit3" "hit2" "hit")
 
 echo "First movie : ${movies[0]}"
 echo "second movie : $movies[2]}"
-echo "first 3 movies : ${movies[@]:0:4:1}"
+echo "first 3 movies : ${movies[@]:0:4}"
---
+++ array2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+movies=("court" "hit-3" "hit-2" "hit-1")
+echo "first movie : ${movies[0]}"
+echo "second movie: ${movies[1]}"
+echo "all movies list: ${movies[@]}"
