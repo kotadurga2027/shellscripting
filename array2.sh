8d92b3295aec7208521455f6255ffd8d0587efb8
RupaSiddana <roopa.siddana@gmail.com>
2025-06-24 21:35:08 +0530

    array2

--- array2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-movies=("court" "hit-3" "hit-2" "hit-1")
-echo "first movie : ${movies[0]}"
-echo "second movie: ${movies[1]}"
-echo "all movies list: ${movies[@]}"
---
+++ special-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "all variables passed to the script:$@"
+echo "number of variable:$#"
+echo "script name:$0"
+echo "current directory:$PWD"
+echo "user running this script:$USER"
+echo "Home directory of users: $HOME"
+echo "PID of the script:$$"
+sleep 10 &
+echo ""PID of last command in background:$!"
