548197323fef07cd5f8f2d4ddd524af0ee1c0e78
RupaSiddana <roopa.siddana@gmail.com>
2025-06-24 22:06:20 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 NUMBER=$1
-if [ $NUMBER -gt 10 ]
+if [ $NUMBER -gt 10]
 then
-   echo "Given Number $NUMBER is greater than 10"
+   echo "Given Number $NUMBER is greaterthan 10"
    
 else
+   echo "Given Number $NUMBER is lessthan to 10"
+fi
+
+if [ $NUMBER -lt 10]
+then
    echo "Given Number $NUMBER is lessthan 10"
+   
+else
+   echo "Given Number $NUMBER is greaterthan to 10"
+fi
+
+if [ $NUMBER -eq 10]
+then
+   echo "Given Number $NUMBER is equal to 10"
+   
+else
+   echo "Given Number $NUMBER is not equal to 10"
+fi
+
+if [ $NUMBER -nq 10]
+then
+   echo "Given Number $NUMBER is not equal to 10"
+   
+else
+   echo "Given Number $NUMBER is equal to 10"
 fi
