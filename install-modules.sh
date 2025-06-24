e69f4bacd1b5aed9832d518f6b426613e1dc4b72
RupaSiddana <roopa.siddana@gmail.com>
2025-06-24 22:51:33 +0530

    install-module

---
+++ install-modules.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+userid=(id -u)
+if [ $userid -ne 0]
+then
+   echo "Error:: Please run with root access"
+else
+   echo "You are running with root access"
+fi
+dnf list installed mysql
+if [ $? -ne 0]
+then
+   echo "Mysql is not installed..... going to install the Mysql"
+   dnf install mysql -y
+   if [ $? -eq 0]
+   then
+      echo "Installing mysql is... sucsess"
+   else
+      echo "Installing my is ... fail"
+else
+   echo "Mysql is already isnatlled no action is pending"
+   exit 1
+fi
+   
