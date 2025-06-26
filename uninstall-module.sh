f8b4faa97ea0719d058df72a387812ff3aa0b3d7
RupaSiddana <roopa.siddana@gmail.com>
2025-06-26 11:24:32 +0530

    functions

--- uninstall-module.sh
+++ uninstall-module.sh
@@ -2,24 +2,22 @@
 userid=$(id -u)
 if [ $userid -ne 0 ]
 then
-   echo "Error:: Please run with root access"
-   exit 1
+    echo "Error:: You are runing the script witout root access"
+    exit 1
 else
-   echo "You are running with root access"
+    echo "You are running the script with root access"
 fi
 dnf list installed mysql
 if [ $? -eq 0 ]
-then
-   echo "Mysql is installed..... going to uninstall the Mysql"
-   dnf remove mysql -y
-   if [ $? -eq 0 ]
-   then
-      echo "Uninstalling mysql is... sucsess"
-   else
-      echo "Uninstalling mysql is ... fail"
-      exit 1
-   fi
+then  
+    echo "Mysql is installed and we are going to uninstall"
+    dnf remove mysql -y
+    if [ $? -eq 0 ]
+    then  
+        echo "Mysql uninstallation is success"
+    else
+        echo "Mysql uninstallation is failure"
+    fi
 else
-   echo "Mysql is not installed no action is pending"
+    echo "Mysql is not installed no need to perform"
 fi
-   
